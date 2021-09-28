e0764a7aa5f06258a1a9ee414ed68a0467d1768e
AndreyZaklitskiy <glockman94@gmail.com>
2021-09-28 11:44:03 +0300

    2017 = > 2021

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+/*slick-slider*/
+$(document).ready(function(){
+  $('.team-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          prevArrow: '<button type="button" class="slickPrev"></button>',
+          nextArrow: '<button type="button" class="slickNext"></button>'
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          prevArrow: '<button type="button" class="slickPrev"></button>',
+          nextArrow: '<button type="button" class="slickNext"></button>'
+        }
+      }
+    ]
+});
+});
+/*adaptive-menu-button-script*/
+$(document).ready(function() {
+  $('.hamburger').click(function () {
+    $('.hamburger').toggleClass('is-active');
+    $('.hamburger-menu').toggleClass('visible');
+  });
+});
